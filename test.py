d95f9a5757abc8e7eb4535f7b898ae11c8c6281b
Lavish Gangwani <lavishgangwani22@gmail.com>
2024-07-01 13:06:11 +0530

    test

--- test.py
+++ test.py
@@ -21,5 +21,27 @@ def check_min(arr):
         return minimum
 
 
-evaluate_test_case(check_min,test)
+#evaluate_test_case(check_min,test)
 
+
+
+"""
+Ques: Find the sum maxinum and minimum in an given array with exactly four out of 5 elements in array
+"""
+arr1 = [1,3,5,7,9]
+def miniMaxSum(arr):
+    sum_  = 0
+    scores = []
+    for i in range(len(arr)):
+        sum_ += arr[i]
+        for j in range(i+1,len(arr)-1):
+            sum_ += arr[j]
+            scores.append(sum_)
+            
+    min_ = min(scores)
+    max_ = max(scores)
+    
+    print(min_ , max_)
+
+miniMaxSum(arr1)
+            
